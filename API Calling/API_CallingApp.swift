a3c52adeb8fcefaf6c0f703d6fa27ccbba079cf2
Stephanie Rocha M <srochamarquez@bsd220.org>
2026-03-12 10:22:34 -0500

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by stephanie rocha marquez on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
