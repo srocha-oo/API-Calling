872b4d008e08b820d1884f0aee052de77dd0b4b6
Stephanie Rocha M <srochamarquez@bsd220.org>
2026-03-19 10:23:26 -0500

    added alert message

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -8,20 +8,15 @@
 import SwiftUI
 
 struct BrandListView: View {
-    
     @State private var products: [Product] = []
-    
+    @State private var showingAlert = false
     var brands: [String] {
         let brandNames = products.compactMap { $0.brand }
         return Array(Set(brandNames)).sorted()
     }
-    
     var body: some View {
-        
         NavigationStack {
-            
             List(brands, id: \.self) { brand in
-                
                 NavigationLink(destination: ProductListView(brand: brand, products: products)) {
                     Text(brand.capitalized)
                 }
@@ -31,17 +26,23 @@ struct BrandListView: View {
         .onAppear {
             loadProducts()
         }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the poetry authors"),
+                  dismissButton: .default(Text("OK")))
+        }
     }
-    
     func loadProducts() {
-        
         let service = MakeupService()
-        
         service.fetchProducts { result in
+            if result.isEmpty {
+                showingAlert = true
+            }
             self.products = result
         }
     }
 }
+
 #Preview {
     BrandListView()
 }
